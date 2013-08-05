1773d98a7f927098c1b0ba85700429f98943bde5
Piotr Solnica <piotr.solnica@gmail.com>
2013-08-05 10:35:41 +0200

    [zsh] update key bindings

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -2,12 +2,18 @@ typeset -g -A key
 
 bindkey -e
 
-bindkey '^H' delete-word
+bindkey '^H' delete-word # iterm
+bindkey '^[[3~' delete-char # tmux
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
+bindkey '^[^[[D' backward-word # iterm
+bindkey '^[[1;3D' backward-word # tmux
 
-bindkey '^[[3~' delete-char
+bindkey '^[^[[C' forward-word # iterm
+bindkey '^[[1;3C' forward-word # tmux
 
-bindkey '^[[H' beginning-of-line
-bindkey '^[[F' end-of-line
+
+bindkey '^[[H' beginning-of-line # iterm
+bindkey '^[[1~' beginning-of-line # tmux
+
+bindkey '^[[F' end-of-line # iterm
+bindkey '^[[4~' end-of-line # tmux
